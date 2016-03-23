36c364854d075de58e08d92805457dfbec8f4b14
Yury <phan@i.ua>
2016-03-23 17:11:34 +0200

    js-04

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var body = document.getElementsByTagName("body");
+var h3 = document.createElement('h3');
+h3.className = "text-center";
+h3.innerHTML = "Тест по программированию";
+
+var form = document.createElement('form');
+form.className="";
+
+var question1 = document.createElement('p');
+question1.className = "";
+question1.innerHTML = "1. Вопрос №1";
+
+document.body.appendChild(h3);
+document.body.appendChild(question1);
